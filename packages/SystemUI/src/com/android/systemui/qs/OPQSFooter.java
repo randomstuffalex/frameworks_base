wahyudi12/android_frameworks_base

OPQSFooter: Fix carrier text glitch when pulling qs
I guess this got accidentally removed in Wave-Project/frameworks_base@3fa1735f3f9e87075c2817c6390bd801c867bf99

Change-Id: Ib980ead9005934ebab65eece1a20cba0e2866b9a
Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

packages/SystemUI/src/com/android/systemui/qs/OPQSFooter.java
@@ -165,6 +165,7 @@ public class OPQSFooter extends LinearLayout {
     private TouchAnimator createFooterAnimator() {
         TouchAnimator.Builder builder = new TouchAnimator.Builder()
                 .addFloat(mEdit, "alpha", 0, 0, 1)
+                .addFloat(mDataUsageView, "alpha", 0, 0, 1)
                 .addFloat(mRunningServicesButton, "alpha", 0, 0, 1);
         if (mIsLandscape) {
             builder = builder.addFloat(mSettingsButton, "alpha", 0, 0, 1)

randomstuffalex/frameworks_base

packages/SystemUI/src/com/android/systemui/qs/OPQSFooter.java
@@ -99,7 +99,8 @@ public class OPQSFooter extends LinearLayout {
     @Nullable
     private TouchAnimator createFooterAnimator() {
         TouchAnimator.Builder builder = new TouchAnimator.Builder()
-                .addFloat(mEdit, "alpha", 0, 0, 1);
+                .addFloat(mEdit, "alpha", 0, 0, 1)
+                .addFloat(mDataUsageView, "alpha", 0, 0, 1);
         if (mIsLandscape) {
             builder = builder.addFloat(mSettingsButton, "alpha", 0, 0, 1)
                     .setStartDelay(0.5f);